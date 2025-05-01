636223a0263c353a2eee355a92fb0bad7df45e04
Kevin Chan <140776786+kevindkchan@users.noreply.github.com>
2025-05-01 17:18:01 +0800

    Add back to top button

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,11 +1,14 @@
 window.addEventListener("scroll", () => {
     const header = document.querySelector('.navbar');
     const sticky = document.querySelector('.sticky');
+    const arrow = document.querySelector('.arrow');
     const headerBottom = header.offsetTop + header.offsetHeight;
 
     if (window.scrollY >= headerBottom) {
         sticky.style.display = 'flex';
+        arrow.style.display = 'flex';
     } else {
         sticky.style.display = 'none';
+        arrow.style.display = 'none';
     }
 });
